1594462224757de455fb5bff8f9ccd0d3346c346
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2022-01-19 16:42:23 +0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='COVID-19-tools',
+      version='0.1',
+      description='Simple up to date data retrieval tools.',
+      author='SpaceD0ge',
+      url='https://github.com/SpaceD0ge/COVID-19-tools',
+      packages=['covidtools'],
+     )
